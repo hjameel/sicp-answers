8dd7939d1af6bda4fa66c43292c60a4a33220cd4
Hanif Jameel <hanif_1989@btinternet.com>
2013-03-09 19:09:12 +0000

    Tidying up ex1.8

--- ch1/ex1.8.rkt
+++ ch1/ex1.8.rkt
@@ -1,30 +1,34 @@
 #lang racket
 ; Exercise 1.8
 
-(define (cube-root x)
-  (cube-root-iter 1.0 100.0 x))
-
-(define (cube-root-iter guess old-guess x)
-  (if (good-enough? guess old-guess x)
-    guess
-    (cube-root-iter (improve guess x) guess x)))
-
-(define (improve guess x)
-  (/ (+ (/ x (square guess))
-        (* 2 guess))
-     3))
-
 (define (square x)
   (* x x))
 
-(define (good-enough? guess old-guess x)
-  (< (abs (- guess old-guess))
-     (/ guess 1000)))
+(define (cube x)
+  (* x x x))
+
+(define (cube-root x)
+  (define (cube-root-iter guess old-guess x)
+    (if (good-enough? guess old-guess x)
+      guess
+      (cube-root-iter (improve guess x) guess x)))
+  (define (good-enough? guess old-guess x)
+    (< (abs (- guess old-guess))
+       (/ guess 1000)))
+  (define (improve guess x)
+    (/ (+ (/ x (square guess))
+          (* 2 guess))
+       3))
+
+  (cube-root-iter 1.0 100.0 x))
+
 
 ; Test
-(< (abs (- (cube-root 27) 3))
-   0.001)
-(< (abs (- (cube-root (* 50 50 50)) 50))
-   0.001)
-(< (abs (- (cube-root (* 0.01 0.01 0.01)) 0.01))
-   0.000001)
+(define (test-cube-root-within-tolerance test-val tolerance)
+  (< (abs (- (cube-root (cube test-val))
+             test-val))
+     tolerance))
+
+(test-cube-root-within-tolerance 3 0.001)
+(test-cube-root-within-tolerance 50 0.001)
+(test-cube-root-within-tolerance 0.01 0.000001)
