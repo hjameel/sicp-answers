d171e15bf386037f1642819cbdfb37fefccabd81
Hanif Jameel <hanif_1989@btinternet.com>
2013-03-09 16:09:48 +0000

    ex1.4 and ex1.5

---
+++ ch1/ex1.4.rkt
@@ -0,0 +1,3 @@
+; Exercise 1.4
+
+; Apply either + or - to the operands, depending on the value of b.
---
+++ ch1/ex1.5.rkt
@@ -0,0 +1,22 @@
+#lang racket
+; Exercise 1.5
+
+(define (p) (p))
+(define (test x y)
+  (if (= x 0) 0 y))
+
+; Applicative-order evaluation:
+; We evaluate the arguments to "test" first, then apply the procedure.
+; i.e. in
+
+;  (test 0 (p))
+
+; (p) is evaluated to (p), which is evaluated to (p) etc. forever.
+
+; Normal-order evaluation:
+; We fully expand the expression, giving:
+(if (= 0 0)
+  0
+  (p))
+
+; which evaluates to 0.
