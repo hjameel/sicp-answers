9162a45f0d12327280af8fa9bc8d16144fef118f
Hanif Jameel <hanif_1989@btinternet.com>
2013-03-16 15:18:37 +0000

    Helper functions.

---
+++ ch2/helpers.rkt
@@ -0,0 +1,10 @@
+#lang racket
+
+(define (equal-lists? list1 list2)
+  (cond ((and (null? list1) (null? list2)) #t)
+         ((or (null? list1) (null? list2)) #f)
+         ((= (car list1) (car list2)) (equal-lists? (cdr list1) (cdr list2)))
+         (else #f)))
+
+;; Provide
+(provide equal-lists?)
