e39f7223de1c1115538f211939d01eaebeb5aa53
Hanif Jameel <hanif_1989@btinternet.com>
2013-03-20 00:25:16 +0000

    exercises 2.40 and 2.41

---
+++ ch2/ex2.41.rkt
@@ -0,0 +1,27 @@
+#lang racket
+;; Exercise 2.41
+
+(require "helpers.rkt")
+
+(define (unique-pairs n)
+  (flatmap (lambda (i)
+             (map (lambda (j) (list i j))
+                  (enumerate-interval 1 (- i 1))))
+           (enumerate-interval 1 n)))
+
+(define (unique-triples n)
+  (flatmap (lambda (ij)
+             (map (lambda (k) (list (car ij) (cadr ij) k))
+                  (enumerate-interval 1 (- (cadr ij) 1))))
+           (unique-pairs n)))
+
+(define (sum-triples s)
+  (filter (lambda (x)
+            (= s
+               (+ (car x)
+                  (cadr x)
+                  (caddr x))))
+          (unique-triples s)))
+
+;; Tests
+(sum-triples 9)
--- ch2/helpers.rkt
+++ ch2/helpers.rkt
@@ -2,9 +2,9 @@
 
 (define (equal-lists? list1 list2)
   (cond ((and (null? list1) (null? list2)) #t)
-         ((or (null? list1) (null? list2)) #f)
-         ((= (car list1) (car list2)) (equal-lists? (cdr list1) (cdr list2)))
-         (else #f)))
+        ((or (null? list1) (null? list2)) #f)
+        ((= (car list1) (car list2)) (equal-lists? (cdr list1) (cdr list2)))
+        (else #f)))
 
 (define (square x)
   (* x x))
@@ -13,12 +13,24 @@
 
 (define (accumulate op initial sequence)
   (if (null? sequence)
-      initial
-      (op (car sequence)
-          (accumulate op initial (cdr sequence)))))
+    initial
+    (op (car sequence)
+        (accumulate op initial (cdr sequence)))))
+
+(define (enumerate-interval a b)
+  (if (> a b)
+    nil
+    (cons a (enumerate-interval (+ a 1) b))))
+
+(define (flatmap proc seq)
+  (accumulate append
+              nil
+              (map proc seq)))
 
 ;; Provide
 (provide equal-lists?
          square
          nil
-         accumulate)
+         accumulate
+         enumerate-interval
+         flatmap)
