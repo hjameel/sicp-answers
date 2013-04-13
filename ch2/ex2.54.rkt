3879b11918793c9c1d7fe4315311fc3f38eff245
Hanif Jameel <hanif_1989@btinternet.com>
2013-04-13 16:04:22 +0100

    ex2.54: equals works for multi element lists

--- ch2/ex2.54.rkt
+++ ch2/ex2.54.rkt
@@ -3,10 +3,16 @@
 
 (define (equal? a b)
   (if (and (pair? a) (pair? b))
-    (eq? (car a) (car b))
+    (if (eq? (car a) (car b))
+      (equal? (cdr a) (cdr b))
+      false)
     (eq? a b)))
 
 ;; Tests
 (equal? 1 1)
+(not (equal? 1 2))
 (equal? '(1) '(1))
 (not (equal? '(1) 1))
+(not (equal? '(1 2) '(1 1)))
+(equal? '(1 1 2) '(1 1 2))
+(not (equal? '(1 1 2) '(1 1)))
