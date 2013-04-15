3fc02212f30870a131d8f3768e54eca7e0b78af2
Hanif Jameel <hanif_1989@btinternet.com>
2013-04-15 11:36:20 +0100

    take the union of sets with different elements

--- ch2/ex2.59.rkt
+++ ch2/ex2.59.rkt
@@ -5,8 +5,12 @@
   (cond ((null? set1) set2)
         ((null? set2) set1)
         ((equal? (car set1) (car set2))
-         (cons (car set1) (union-set (cdr set1) (cdr set2))))
-        (else (union-set (cdr set1) (cdr set2)))))
+         (cons (car set1)
+               (union-set (cdr set1) (cdr set2))))
+        (else
+          (cons (car set1)
+                (cons (car set2)
+                      (union-set (cdr set1) (cdr set2)))))))
 
 ;; Tests
 (equal? (union-set '(1) '())
@@ -15,5 +19,7 @@
         '(1))
 (equal? (union-set '(2) '(2))
         '(2))
-(equal? (union-set '(2, 3) '(2))
-        '(2, 3))
+(equal? (union-set '(2 3) '(2))
+        '(2 3))
+(equal? (union-set '(1) '(2))
+        '(1 2))
