c39f47944024bada48980172b51667ddf570f3ea
Hanif Jameel <hanif_1989@btinternet.com>
2013-04-15 13:26:09 +0100

    corrected ex2.59 -> now works for unordered sets

--- ch2/ex2.59.rkt
+++ ch2/ex2.59.rkt
@@ -1,37 +1,26 @@
 #lang racket
 ;; Exercise 2.59
 
+(define (element-of-set? x set)
+  (cond ((null? set) false)
+        ((equal? x (car set)) true)
+        (else (element-of-set? x (cdr set)))))
+
+(define (adjoin-set x set)
+  (if (element-of-set? x set)
+      set
+      (cons x set)))
+
 (define (union-set set1 set2)
   (cond ((null? set1) set2)
         ((null? set2) set1)
-        ((equal? (car set1) (car set2))
-         (cons (car set1)
-               (union-set (cdr set1) (cdr set2))))
+        ((element-of-set? (car set1) set2)
+         (union-set (cdr set1) set2))
         (else
-          (cons (car set1)
-                (cons (car set2)
-                      (union-set (cdr set1) (cdr set2)))))))
-
-(define (union-set-iter set1 set2)
-  (define (union-set-agg set1 set2 agg)
-    (cond ((null? set1)  (append set2 agg))
-          ((null? set2)  (append set1 agg))
-          ((equal? (car set1) (car set2))
-           (union-set-agg
-             (cdr set1)
-             (cdr set2)
-             (cons (car set1) agg)))
-          (else
-           (union-set-agg
-             (cdr set1)
-             (cdr set2)
-             (cons (car set1) (cons (car set2) agg))))))
-
-  (union-set-agg set1 set2 '()))
+         (union-set (cdr set1) (adjoin-set (car set1) set2)))))
 
 
 ;; Tests
-;; Recursive solution
 (equal? (union-set '(1) '())
         '(1))
 (equal? (union-set '(1) '(1))
@@ -39,18 +28,6 @@
 (equal? (union-set '(2) '(2))
         '(2))
 (equal? (union-set '(2 3) '(2))
-        '(2 3))
-(equal? (union-set '(1) '(2))
-        '(1 2))
-
-;; Iterative solution
-(equal? (union-set-iter '(1) '())
-        '(1))
-(equal? (union-set-iter '(1) '(1))
-        '(1))
-(equal? (union-set-iter '(2) '(2))
-        '(2))
-(equal? (union-set-iter '(2 3) '(2))
         '(3 2))
-(equal? (union-set-iter '(1) '(2))
+(equal? (union-set '(1) '(2))
         '(1 2))
